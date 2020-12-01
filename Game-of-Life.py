993103bf5fa4cb793873ec36f0ba7d880c77381a
n-arms <armstrong@Armstrongs-MacBook.local>
2020-12-01 10:16:25 -0500

    Add game of life class and settable to Tile
    * inherits from Frame to hold tiles
    * creates tiles on instantiation
    * settable allows a tile to be changed by clicking

--- Game-of-Life.py
+++ Game-of-Life.py
@@ -10,6 +10,7 @@ class Tile(Canvas):
         # canvas purely to show state using the background
         Canvas.__init__(self, master, width=10, height=10, bg='white', bd=0)
         self.state = False # a boolean to represent whether a tile is alive
+        self.settable = True # if true, the tile can be toggled by clicking
         
     def get_state(self):
         '''Tile.get_state()
@@ -34,9 +35,10 @@ class Tile(Canvas):
     def set_state(self, event):
         '''Tile.set_state(event)
         handler method for game initiation'''
-        self.state = self.state ^ True # toggles state
-        self.update_display()
-
+        if self.settable:
+            self.state = self.state ^ True # toggles state
+            self.update_display()
+            
     def update_display(self):
         '''Tile.update_disply()
         used to update every tile at once'''
@@ -44,6 +46,23 @@ class Tile(Canvas):
             self['bg'] = 'black'
         else:
             self['bg'] = 'white'
+
+class GameOfLife(Frame):
+    '''frame for the game of life'''
+    def __init__(self, master, width, height):
+        '''GameOfLife(master, name) -> GameOfLife
+        create a new game of life
+        width and height are both measured in tiles'''
+        # set up Frame object
+        Frame.__init__(self, master)
+        self.grid()
+        self.tiles = {}
+        for i in range(width):
+            for j in range(height):
+                self.tiles[(i, j)] = Tile(master)
+                self.tiles[(i, j)].grid()
+        
+        
 
     
         
