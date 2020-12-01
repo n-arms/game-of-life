7a9ae3dadc09f4e546be6e5ef3f9d45dfb206b98
n-arms <armstrong@Armstrongs-MacBook.local>
2020-12-01 09:56:10 -0500

    Add state getter and setter
    * a simple getter to return the boolean state
    * a setter that can be passed a list of neighbouring tiles to determine whether to live

--- Game-of-Life.py
+++ Game-of-Life.py
@@ -11,6 +11,27 @@ class Tile(Canvas):
         Canvas.__init__(self, master, width=10, height=10, bg='white', bd=0)
         self.state = False # a boolean to represent whether a tile is alive
         
+    def get_state(self):
+        '''Tile.get_state()
+        returns the Tile.state attribute'''
+        return self.state
 
+    def change_state(self, tiles):
+        '''Tile.change_state(tiles)
+        changes the tile's current state
+        tiles should be a list of the surrounding tiles'''
+        # calculate the number of neighbouring tiles are alive
+        neighbour_count = sum([1 for i in tiles if i.get_state()])
+        # triggers on the two death conditions of overcrowding or underpop
+        if neighbour_count == 1 or neighbour_count >= 4:
+            self.state = False
+        # triggers on the reproduction condition
+        elif neightbour_count == 3 and not self.state:
+            self.state = True
+
+        # all other states result in no change
+
+    
+        
         
         
